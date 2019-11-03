007b4b1a61f0db9b312a87d61096d9f99e7064bb
nouaryk <nouary@live.com>
2019-11-03 01:16:34 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var contenedorModal = document.getElementsByClassName('fc-ab-root');
+
+setTimeout(function() {
+    contenedorModal[0].remove();
+    document.body.style = 'overflow: scroll';
+}, 2000);
